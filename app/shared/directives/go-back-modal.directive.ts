4829fd29cb916d75fac8f2d6435f35ab711c12a3
Toma Nikolov <toma.nikolov@telerik.com>
2018-09-10 10:41:32 +0300

    Back navigation in tabs.

--- app/shared/directives/go-back-modal.directive.ts
+++ app/shared/directives/go-back-modal.directive.ts
@@ -1,31 +1,49 @@
 import { Directive, Injector, OnInit } from "@angular/core";
 import { ModalDialogParams } from "nativescript-angular/modal-dialog";
 import { RouterExtensions } from "nativescript-angular/router";
-import * as app from "tns-core-modules/application/application";
 import { Page } from "tns-core-modules/ui/page/page";
 import { ActionItem, NavigationButton } from "tns-core-modules/ui/action-bar/action-bar";
+import { ActivatedRoute } from "@angular/router";
+import { path } from "tns-core-modules/file-system/file-system";
+import * as app from "tns-core-modules/application/application";
+
+import { NavigationService } from "~/core/services/navigation.service";
 
 @Directive({
-    selector: "[modalGoBack]",
-
+    selector: "[goBack]",
 })
 export class GoBackModalDirective implements OnInit {
     private _modalDialogParams: ModalDialogParams;
 
     constructor(private _injector: Injector,
         private _routerExtensions: RouterExtensions,
-        private page: Page) { }
+        private page: Page,
+        private _activatedRoute: ActivatedRoute,
+        private _navigationService: NavigationService, ) {
+    }
 
     ngOnInit(): void {
         this.addNavButton();
     }
 
     public goBack() {
-        if (this.isInsideModalDialog()) {
-            if (this._routerExtensions.canGoBackToPreviousPage()) {
-                this._routerExtensions.backToPreviousPage();
-            } else {
+        if (this._routerExtensions.canGoBackToPreviousPage()) {
+            this._routerExtensions.backToPreviousPage();
+        } else {
+            if (this.isInsideModalDialog()) {
                 this._modalDialogParams.closeCallback();
+            } else if (this._routerExtensions.canGoBack()) {
+                this._activatedRoute.params.subscribe(params => {
+                    const backPath = "..";
+                    let navigationPath = backPath;
+                    for (const key in params) {
+                        if (params.hasOwnProperty(key)) {
+                            navigationPath = path.join(navigationPath, backPath);
+                        }
+                    }
+
+                    this._navigationService.relativeRouterNavigation([navigationPath], this._activatedRoute);
+                });
             }
         }
     }
